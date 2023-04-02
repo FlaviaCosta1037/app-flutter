7d572f28a3805a42cb488e1e3fce1533833b9e8b
Flávia Costa <88057693+FlaviaCosta1037@users.noreply.github.com>
2023-04-02 20:47:24 -0300

    Ajuste Icons buttons, footer e appbar

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
-        backgroundColor: Color.fromRGBO(244, 244, 244, 0.863),
+        backgroundColor: Color.fromRGBO(244, 244, 244, 2),
         extendBody: true,
         extendBodyBehindAppBar: true,
         appBar: AppBar(
+          leading: Icon(
+            Icons.soup_kitchen_outlined,
+            size: 50,
+            color: Color.fromARGB(167, 12, 11, 30),
+          ),
           centerTitle: true,
+          // leading: Image.asset('assets/images/icons8-panela-50.png'),
           backgroundColor: Color.fromRGBO(204, 166, 28, 0.70),
           titleTextStyle: const TextStyle(
             color: Color.fromARGB(206, 26, 12, 11),
@@ -28,51 +34,71 @@ class MyApp extends StatelessWidget {
           title: const Text(
             'Temperos do Sertão',
           ),
+          actions: [
+            Icon(
+              Icons.settings,
+              color: Color.fromARGB(153, 28, 16, 247),
+            ),
+          ],
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                Icon(
-                  Icons.edit_square,
-                  size: 128,
-                  color: Color.fromRGBO(204, 165, 28, 30),
-                ),
-                Icon(
-                  Icons.format_list_bulleted_outlined,
-                  size: 128,
-                  color: Color.fromRGBO(204, 165, 28, 30),
-                ),
-                Icon(
-                  Icons.logout_outlined,
-                  size: 128,
-                  color: Color.fromRGBO(204, 165, 28, 30),
-                ),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                    icon: Icon(Icons.edit_square,
+                        size: 60, color: Color.fromRGBO(204, 166, 28, 0.70)),
+                    onPressed: () {
+                      print("botão pressionado");
+                    }),
+                IconButton(
+                    icon: Icon(
+                      Icons.format_list_bulleted_outlined,
+                      color: Color.fromRGBO(204, 166, 28, 0.70),
+                      size: 60,
+                    ),
+                    onPressed: () {
+                      print("botão pressionado");
+                    }),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                    icon: Icon(Icons.person,
+                        size: 60, color: Color.fromRGBO(204, 166, 28, 0.70)),
+                    onPressed: () {
+                      print("botão pressionado");
+                    }),
+                IconButton(
+                    icon: Icon(
+                      Icons.restaurant_menu_rounded,
+                      color: Color.fromRGBO(204, 166, 28, 0.70),
+                      size: 60,
+                    ),
+                    onPressed: () {
+                      print("botão pressionado");
+                    }),
               ],
             ),
           ],
         ),
-
         //Footer
         bottomNavigationBar: BottomAppBar(
             color: Color.fromRGBO(204, 165, 28, 30),
             child: Container(
-              alignment: Alignment.center,
               height: 49,
-              child: Row(children: [
-                Container(
-                  child: const Text(
-                    '© 2023 - AuroraTech',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                )
-              ]),
+              child: Center(
+                  child: Text(
+                'Temperos do Sertão | 2023 Copyright © AuroraTech. Todos direitos reservados.',
+                style: TextStyle(
+                    fontSize: 13,
+                    color: Color.fromARGB(207, 12, 11, 30),
+                    fontWeight: FontWeight.bold),
+              )),
             )),
       ),
     );
